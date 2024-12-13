db1bcaeb742dce9732362afa4d76cc0058b89010
José Ignacio Puig <ipnachoip@gmail.com>
2024-12-13 23:56:52 +0000

    Pronto

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,69 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	<div className=" container-fluid">
+		<footer className="footer d-flex flex-wrap justify-content-between">
+
+			<div className="footer-izq mb-3 text-center text-md-start">
+
+				<div className="titulo">
+					RoadUp Academia de manejo
+				</div>
+
+				<div className="parrafo">
+					<p>Impulsando tu camino hacia la excelencia en el manejo.</p>
+					<p>Aprende, practica y avanza con confianza.</p>
+				</div>
+
+				<div className="location p-1">
+					<i className="fa-solid fa-location-dot"></i> Francia 24
+				</div>
+
+				<div className="sobre p-1">
+					<i className="fa-solid fa-envelope"></i> consultas@roadup.com.uy
+				</div>
+
+				<div className="phone p-1">
+					<i className="fa-solid fa-phone"></i> 598 202-4792
+				</div>
+
+			</div>
+
+			<div className="footer-centro mb-3 text-center">
+				<div className="titulo">
+					Pregúntanos
+				</div>
+				<form className=" row g-3">
+					<div className=" col-12 ">
+						<input className="form-control form-control-sm mb-2" id="nombre" placeholder="Nombre" required />
+						<input className="form-control form-control-sm mb-2" type="text" id="telefono" placeholder="Teléfono" />
+						<input className="form-control form-control-sm mb-2" type="email" placeholder="Email" required />
+						<textarea className="form-control form-control-sm mb-2" id="comen" rows="3" placeholder="Comentarios"></textarea>
+						<button className="boton btn btn-primary d-flex justify-content-end" type="submit">Enviar</button>
+					</div>
+				</form>
+
+				
+			</div>
+
+			<div className="footer-der mb-3 text-center text-md-end">
+				<div className="titulo">
+					Compañía
+				</div>
+
+				<div className="texto">
+					<p>About Us</p>
+					<p>Careers</p>
+					<p>FAQs</p>
+					<p>Contact Us</p>
+				</div>
+				<div className="social-icons">
+					<i className="fa-brands fa-square-instagram mx-2"></i>
+					<i className="fa-brands fa-square-twitter mx-2"></i>
+					<i className="fa-brands fa-facebook mx-2"></i>
+				</div>
+			</div>
+
+		</footer>
+	</div>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,74 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className=" navbar  body-tertiary ">
+
+			<div className=" container-fluid ">
+
+				<div className=" container-fluid d-flex justify-content-between ">
+					<h1 className="navbar-brand  ml-4 ">RideUp</h1>
+
+					<div className="d-flex justify-content-between mr-4">
+						{
+							true && 
+							<ul className="list-group list-group-horizontal">
+							<li className="list-group-item">
+								<NavLink to="/login">Iniciar Sesión</NavLink>
+							</li>
+							<li className="list-group-item">
+								<NavLink to="/register">Registro</NavLink>
+							</li>
+							</ul>
+						}
+						
+						{
+							false && 
+							<>
+							<h3 className="navbar-brand text-primary-emphasis d-flex align-items-center ms-auto"> Hola User </h3>
+
+
+							<button className="navbar-toggler bg-light btn-outline-primary fa-regular fa-user me-2"></button>
+	
+							<button className="navbar-toggler bg-light btn-outline-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+								<span className="fa-solid fa-bars "></span>
+							</button>
+							</>
+						}
+						
+					</div>
+				</div>
+
+				<div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+					<div className="offcanvas-header">
+						<h5 className="offcanvas-title" id="offcanvasNavbarLabel">RideUp</h5>
+						<button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+					</div>
+					<div className="offcanvas-body">
+						<ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+							<li className="nav-item">
+								<a className="nav-link" href="#">Mis clases</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">Agendar clase</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">Mi usuario</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">Cerrar sesión</a>
+							</li>
+
+						</ul>
+
+					</div>
 				</div>
 			</div>
+
 		</nav>
+
+
+
 	);
 };
